33fb56777919bb6de6180c232b55f177ee895c86
Demix123 <max.demixxx@gmail.com>
2019-03-28 14:53:20 +0200

    krosshop

--- size.js
+++ size.js
@@ -1,15 +1,11 @@
 let size = document.querySelectorAll('.size-value');
-
+let a;
 for(let i = 0; i < size.length; i++){
     size[i].addEventListener('click', function(){
-        size[i].style.backgroundColor = '#919191';
-        size[i].style.color = 'white';
-    })
-
-    size[i].addEventListener('click', function(){
-        if (size[i].style.backgroundColor == '#919191' && size[i].style.color == 'white'){
-            size[i].style.backgroundColor = 'white';
-            size[i].style.color = 'black';
+        if(a >= 0 && a < size.length){
+            size[a].classList.remove('size-active');
         }
+        this.classList.add('size-active');
+        a = i;
     })
 }
