e2c95b9c18fffdf8577640c0964107ffe55b816c
Demix123 <max.demixxx@gmail.com>
2019-04-06 22:55:17 +0300

    hypeshop

---
+++ slider.js
@@ -0,0 +1,10 @@
+jQuery( document ).ready(function( $ ) {
+    $( '#my-slider' ).sliderPro({
+        height: 500,
+        arrows: true,
+        buttons: false,
+        waitForLayers: true,
+        autoplay: true,
+        autoScaleLayers: false
+    });
+});
