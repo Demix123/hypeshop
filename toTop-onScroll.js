63e176f6119137d25b78867997f264663950b6dd
Demix123 <max.demixxx@gmail.com>
2019-04-09 18:46:39 +0300

    hypeshop

--- toTop-onScroll.js
+++ toTop-onScroll.js
@@ -1,5 +1,9 @@
 window.onscroll = function(){
     let btnToTop = document.querySelector('.arrow-frame');
-    btnToTop.style.opacity = '1';
-    btnToTop.style.transition = '0.5s';
+    btnToTop.style.opacity = '0.9';
+    btnToTop.style.transition = '1s';
+
+    btnToTop.addEventListener('click', function(){
+        this.classList.add = 'scroll-bevavior';
+    })
 }
