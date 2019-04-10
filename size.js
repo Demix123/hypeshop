f02aeb13386d049098c0ed9ca98e6344b8248148
klvsov <klvsov@gmail.com>
2019-04-10 14:01:09 +0300

    update form

--- size.js
+++ size.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+window.onload = function(){
     let size = document.querySelectorAll('.size-value');
     let a;
     var selected_size;
@@ -45,71 +45,85 @@ window.onload = function () {
                     overlay.style.display = 'none';
                 })
 
-                let btn_submit = document.querySelector('#modal_form input[type=submit]');
-                btn_submit.addEventListener('click', function() {
-                    let form = document.forms[1];
-                    let userName = document.forms[1].elements['user-name'];
-                    let tel = document.forms[1].elements['tel'];
-                    let region = document.forms[1].elements['region'];
-                    let town = document.forms[1].elements['town'];
-                    let post = document.forms[1].elements['post'];
-                    let isConfirm = document.getElementById('confirm');
-                    let warning_name = document.querySelector('.w-name');
-                    let warning_tel = document.querySelector('.w-tel');
-                    let warning_region = document.querySelector('.w-region');
-                    let warning_town = document.querySelector('.w-town');
-                    let warning_post = document.querySelector('.w-post');
-                    let warning_check = document.querySelector('.w-check');
+                let btn_submit = document.querySelector('.pay');
 
-                    console.log(region);
+                btn_submit.addEventListener('click', stop, false);
 
-                    if (!isConfirm.checked) {
-                        warning_check.textContent = "Потрібно підтвердити замовлення!!!";
-                    } else {
-                        warning_check.textContent = "";
-                    }
+                let form_submit = document.forms[1];
+                let userName = document.forms[1].elements['user-name'];
+                let tel = document.forms[1].elements['tel'];
+                let region = document.forms[1].elements['region'];
+                let town = document.forms[1].elements['town'];
+                let post = document.forms[1].elements['post'];
+                let isConfirm = document.getElementById('confirm');
+                let warning_name = document.querySelector('.w-name');
+                let warning_tel = document.querySelector('.w-tel');
+                let warning_region = document.querySelector('.w-region');
+                let warning_town = document.querySelector('.w-town');
+                let warning_post = document.querySelector('.w-post');
+                let warning_check = document.querySelector('.w-check');
+              
 
-                    for (let i = 0; i < form.length; i++) {
-                        if (form[i].classList.contains('is-invalid')) {
-                            form[i].classList.remove('is-invalid');
-                        }
-                    }
 
+                userName.addEventListener('input', function(){
                     if (userName.value.length < 5 || !userName.value.includes(' ')) {
                         userName.classList.add('is-invalid');
-                        warning_name.textContent = "Правильно заповніть своє ім'я та прізвище!"
+                        warning_name.textContent = "Правильно заповніть своє ім'я та прізвище!";
                     } else {
+                        userName.classList.remove('is-invalid');
                         warning_name.textContent = "";
                     }
-
+                })
+                
+                tel.addEventListener('input', function(){
                     if (tel.value.length <= 12 || !tel.value.includes('+')) {
                         tel.classList.add('is-invalid');
-                        warning_tel.textContent = "Правильно заповніть свій номер телефону!"
-                    } else {
-                        warning_tel.textContent = "";
+                        warning_tel.textContent = "Правильно заповніть свій номер телефону!";
+                    } else{
+                        tel.classList.remove('is-invalid');
+                        warning_tel.textContent = "";  
                     }
+                });
 
+                region.addEventListener('input', function(){
                     if (region.value.length <= 2) {
                         region.classList.add('is-invalid');
-                        warning_region.textContent = "Правильно заповніть область!"
-                    } else {
+                        warning_region.textContent = "Правильно заповніть область!";
+                    } else{
+                        region.classList.remove('is-invalid');
                         warning_region.textContent = "";
                     }
+                });
 
+                town.addEventListener('input', function(){
                     if (town.value.length <= 2) {
                         town.classList.add('is-invalid');
-                        warning_town.textContent = "Правильно заповніть місто доставки!"
-                    } else {
+                        warning_town.textContent = "Правильно заповніть місто доставки!";
+                    } else{
+                        town.classList.remove('is-invalid');
                         warning_town.textContent = "";
                     }
+                });
 
+                post.addEventListener('input', function(){
                     if (isNaN(post.value) || post.value.length < 1) {
                         post.classList.add('is-invalid');
-                        warning_post.textContent ="Правильно заповніть номер відділення!"
-                    } else {
-                        warning_post.textContent = "";
+                        warning_post.textContent ="Правильно заповніть номер відділення!";
+                    } else{
+                        post.classList.remove('is-invalid');
+                        warning_post.textContent ="";
                     }
+                });   
+                
+                btn_submit.addEventListener('click', function(){
+                    if( (isConfirm.checked) && (!isNaN(post.value)) && !(post.value.length < 1) && !(town.value.length <= 2) && !(region.value.length <= 2) && !(tel.value.length <= 12) && (tel.value.includes('+')) && !(userName.value.length < 5) && (userName.value.includes(' '))){
+                        warning_check.textContent = "";
+                        form_submit.submit();
+                    } else {
+                        warning_check.textContent = "Потрібно підтвердити замовлення та заповнити всі поля!!!";
+                    }    
                 })
+                
             }
         }
     })
