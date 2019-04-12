d23690c69ac1ae65b4fb1be616ca20bffd567477
Demix123 <max.demixxx@gmail.com>
2019-04-12 23:59:36 +0300

    hypeshop

--- btnToTop-onTop.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-    let btnToTop = document.querySelector('.fa-chevron-up');
-
-    if ((clientX,clientY) == 0 && (pageX,pageY) == 0){
-        btnToTop.style.opacity = '0';
-        btnToTop.style.transition = '0.5s';
-    }
-}
